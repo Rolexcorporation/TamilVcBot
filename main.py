fc1136bcee2fe6d795066b01a6c21f7e3ebdf263
Rolexcorporation <121960094+Rolexcorporation@users.noreply.github.com>
2023-01-05 00:46:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from os import environ
 # import logging
 from pyrogram import Client, idle
 
-API_ID = int(environ["API_ID"])
-API_HASH = environ["API_HASH"]
-SESSION_NAME = environ["SESSION_NAME"]
+API_ID = int(environ["26885114"])
+API_HASH = environ["41bb6d1f706da3bfbe95a820f427ae78"]
+SESSION_NAME = environ["5832249400:AAHz02Tf31BGOsXT7O6i-6PoEP1eUTPPdts"]
 
 plugins = dict(
     root="plugins",
@@ -18,8 +18,8 @@ plugins = dict(
 app = Client(SESSION_NAME, API_ID, API_HASH, plugins=plugins)
 # logging.basicConfig(level=logging.INFO)
 app.start()
-print('>>> USERBOT STARTED by @TamilBots')
+print('>>> USERBOT STARTED by @madanopcon')
 idle()
 app.stop()
-print('\n>>> USERBOT STOPPED by @TamilBots')
+print('\n>>> USERBOT STOPPED by @madanopcon')
 
